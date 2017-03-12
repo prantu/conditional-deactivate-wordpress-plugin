bec2d634419baa66a5f24a8b3d5da32167852430
Tamal Sen <pleasant.art.mun@gmail.com>
2017-03-12 17:46:37 +0600

    Added Comments

--- load.php
+++ load.php
@@ -9,8 +9,11 @@ add_filter( 'option_active_plugins', 'disable_wppas_plugin' );
 function disable_wppas_plugin($plugins){
 
     if(strpos($_SERVER['REQUEST_URI'], '.*/amp/') === FALSE AND strpos($_SERVER['REQUEST_URI'], '/wp-admin/') === FALSE) {
-
-        $key = array_search( 'wppas/wppas.php' , $plugins );
+/* 
+Here ./amp/ with wildcard (*) to eliminate any url which ends with /amp/. 
+You can use your own post or page
+*/
+        $key = array_search( 'wppas/wppas.php' , $plugins ); // wppas/wppas.php is the folder name and the php file to block
 
         if ( false !== $key ) {
             unset( $plugins[$key] );
